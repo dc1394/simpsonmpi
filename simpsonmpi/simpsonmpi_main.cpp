283e678ad00b2b45ac9e7cfad170e0af6ae2250c
dc1394 <dc1394@hotmail.com>
2016-07-01 08:50:11 +0900

    細かい修正

---
+++ simpsonmpi/simpsonmpi_main.cpp
@@ -0,0 +1,15 @@
+﻿#include "simpson_integral.h"
+#include <iostream>
+
+namespace {
+    static auto constexpr N = 100000000;
+}
+
+int main()
+{
+    auto const func = myfunctional::make_functional([](double x) { return 1.0 / (2.0 * std::sqrt(x)); });
+    simpsonmpi::SimpsonMpi<decltype(func)> s(func, N, 1.0, 4.0);
+    s();
+    
+    return 0;
+}
